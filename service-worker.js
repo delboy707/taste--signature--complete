471356e52b4c194a74ecbb2d86b84bceef50ae92
Derek Roberts <derek@qepmarketingai.com>
2026-05-07 10:59:47 +0200

    Bump SW version to 3.4.18-slider-defaults

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Provides offline caching and improved performance
 
 // Version: Update this when making significant changes
-const VERSION = '3.4.17-lexicon-align';
+const VERSION = '3.4.18-slider-defaults';
 const CACHE_NAME = `taste-signature-${VERSION}`;
 
 // Files to cache for offline use
